cda1c1ce329aa00837cd12cfb4f14210faa3a1ef
Alberto Anguita <alberto.anguita@gmail.com>
2015-02-05 23:08:17 +0100

    bio and work info shown

---
+++ js/resumeBuilder.js
@@ -0,0 +1,90 @@
+
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Decosoro Crespo Laboratory, UPM",
+            "title": "Developer",
+            "location": "Facultad de Informatica, UPM, Madrid",
+            "dates": "2005-2006",
+            "description": "Java developer of different IA related applications"
+        },
+        {
+            "employer": "Biomedical Informatics Group, UPM",
+            "title": "Researcher",
+            "location": "Facultad de Informatica, UPM, Madrid",
+            "dates": "Since 2006",
+            "description": "Biomedical informatics researcher, managing different european-level research projects and completing my PhD"
+        }
+    ]
+};
+
+var projects = [
+    {
+        "title": "ACGT",
+        "dates": "2006-2010",
+        "description": "European project on heterogeneous data integration for cancer research",
+        "images": ["tbd", "tbd"]
+    },
+    {
+        "title": "p-medicine",
+        "dates": "2011-",
+        "description": "European project on semantic technologies for the development of techniques for achieving personalized medicine",
+        "images": ["tbd", "tbd"]
+    }
+];
+
+var bio = {
+    "name": "Alberto Anguita",
+    "role": "Computer scientist, researcher",
+    "welcomeMessage": "Hi!",
+    "contacts": {
+        "mobile": "625-725-961",
+        "email": "alberto.anguita@gmail.com",
+        "github": "albertoanguita",
+        "location": "Madrid"
+    },
+    "skills": ["Java", "Object oriented programming", "program design", "Ontologies", "RDF", "HTML", "Javascript"],
+    bioPic: "tbd"
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "UCM",
+            "location": "Madrid",
+            "degree": "Physics",
+            "majors": ["Three years of the physics degree completed (2004)"],
+            "dates": "2000-2004",
+            "url": "https://www.ucm.es/"
+        },
+        {
+            "name": "UPM",
+            "location": "Madrid",
+            "degree": "Computer Science",
+            "majors": ["Graduated in Computer Science (2006)", "PhD in Artificial Intelligence (2012)"],
+            "dates": "2004-",
+            "url": "https://www.upm.es/"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Introduction to Android programming",
+            "school": "Miriada",
+            "dates": 2013,
+            "url": "https://www.miriadax.net/web/android_programacion"
+        },
+        {
+            "title": "Introduction to data science",
+            "school": "Coursera",
+            "dates": 2013,
+            "url": "https://www.coursera.org/course/datasci"
+        },
+        {
+            "title": "Front-end web development nanodegree",
+            "school": "Udacity",
+            "dates": 2015,
+            "url": "https://www.udacity.com/course/nd001"
+        }
+    ]
+};
